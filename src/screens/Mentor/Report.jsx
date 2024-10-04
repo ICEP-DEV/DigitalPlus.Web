67d52d5fa57f407b6cc82cabe79c8168aaf5d8d0
chisa-sifiso <221455436@tut4life.ac.za>
2024-10-04 07:53:57 +0200

    Update on the report

--- src/screens/Mentor/Report.jsx
+++ src/screens/Mentor/Report.jsx
@@ -45,7 +45,7 @@ const Report = () => {
             <Header />
             <SideBar />
             
-      
+       <h1>Report</h1>
         <div className={styles['report-form-container']}>
             <form onSubmit={handleSubmit}>
                 <div className={styles['report-form-row']}>
